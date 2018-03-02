2dd90e3c68a6da9289f32caa8189d2494d006622
Carlo Abalos <carloabalos24@gmail.com>
2018-03-02 10:30:33 +0800

    Mongoose Part 2

--- index.js
+++ index.js
@@ -6,27 +6,32 @@ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-const connect = mongoose.connect(url, {
-	useMongoClient: true
-});
+mongoose.connect(url);
 
-connect.then((db) => {
+mongoose.connection.then((db) => {
 
 	console.log('Connected correctly to server');
 
 
-	var newDish = Dishes({
+	Dishes.create({
 		name: 'Carlo',
 		description: 'test'
+	})
+	.then((dish) => {
+		console.log(dish);
+
+		return Dishes.find({}).exec();
+	})
+	.then((dishes) => {
+		console.log(dishes);
+
+
+		return db.collection('dishes').drop();
+	})
+	.then(() => {
+		return db.close();
+	})
+	.catch((err) => {
+		console.log(err);
 	});
-
-	newDish.save()
-		.then((dish) => {
-			console.log(dish);
-
-			return Dishes.find({}).exec();
-		})
-		.then((dishes) => {
-			console.log(dishes);
-		});
 });
